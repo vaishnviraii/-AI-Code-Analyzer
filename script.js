b1d26470d351529f205729a3f85147f0cb27c2ee
Vaishnavi Rai <156286703+vaishnviraii@users.noreply.github.com>
2025-08-29 20:16:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ async function Analyze() {
 
     try {
         // ✅ Correct backend route
-        const res = await fetch("https://ai-generated-code-analyzer.onrender.com/", {
+        const res = await fetch("https://ai-code-analyzer-pied.vercel.app/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -53,4 +53,5 @@ async function Analyze() {
 }
 
 window.Analyze = Analyze;
+
 
